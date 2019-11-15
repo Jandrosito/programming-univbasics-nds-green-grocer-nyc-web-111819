169a60ca3ed41a78b555cafa5d2202ec49c7fbf8
Edward Castillo <castilloedwarda@gmail.com>
2019-11-15 07:09:03 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,7 +1,12 @@
 def find_item_by_name_in_collection(name, collection)
   # Implement me first!
-  #
-  # Consult README for inputs and outputs
+  
+  
+  if !collection[:name[name]]
+    return collection[:name[name]]
+  else 
+    nil 
+  end
 end
 
 def consolidate_cart(cart)
