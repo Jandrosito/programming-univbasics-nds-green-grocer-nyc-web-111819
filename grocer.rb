6b6425f883d7690d153a4ee66296b741f4d5629b
Edward Castillo <castilloedwarda@gmail.com>
2019-11-15 08:08:14 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -4,10 +4,12 @@ BIG_PURCHASE_DISCOUNT_RATE = 0.10
 def find_item_by_name_in_collection(name, collection)
   i = 0
   while i < collection.length do
-    return collection[i] if name === collection[i][:item]
-    i += 1
+  if collection[i][:item] == name
+    return collection[i]
   end
-  nil
+  i += 1
+end
+nil
 end
 
 def consolidate_cart(cart)
